21febce13c80d70237731a8c534d9e7ffdf3f5ed
Egor <you@example.com>
2021-09-12 20:19:26 +0300

    dobavlen controller registracii i model user

--- models/user.py
+++ models/user.py
@@ -4,8 +4,17 @@ from config import mysql
 class User(object):
 
     @staticmethod
-    def register() -> None:
-        pass
+    def register(login, password, email, regdate, status) -> None:
+        connection = mysql.get_db()
+        cursor = mysql.get_db().cursor()   #sozdaem cursor
+        sql_query = """
+            insert into users (login, password, email, regdate, status)
+            values (%s, %s, %s, %s, %s)
+        """
+        cursor.execute(sql_query, (login, password, email, regdate, status))
+        connection.commit()
+        cursor.close()
+        connection.close()
 
     @staticmethod
     def login() -> None:
